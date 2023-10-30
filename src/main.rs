bfcbd2602b42095e69b3545281affc10a1c1a678
Michael Winkelmann <michael@winkelmann.site>
2023-10-30 14:11:35 +0100

    read_csv

--- src/main.rs
+++ src/main.rs
@@ -21,24 +21,77 @@ struct Payment {
 }
 
 
+type DateTime = chrono::DateTime<chrono::Utc>;
+
+#[derive(Debug, serde::Deserialize)]
+struct WorklogRecord {
+    start: String,
+    hours: f32,
+    rate: f32,
+    message: String
+}
+
+struct Worklog {
+    start_date: DateTime,
+    end_date: DateTime,
+    records: Vec<WorklogRecord>
+}
+
+impl Worklog {
+
+    pub fn new() -> Self {
+        Self {
+            start_date: DateTime::MAX_UTC,
+            end_date: DateTime::MIN_UTC,
+            records: Vec::new()
+        }
+    }
+
+    pub fn from_csv(reader: impl std::io::Read) -> Result<Self, Box<dyn std::error::Error>> {
+        let mut rdr = csv::Reader::from_reader(reader);
+        let mut worklog = Self::new();
+        
+        for result in rdr.deserialize() {
+            // Notice that we need to provide a type hint for automatic
+            // deserialization.
+            let record: WorklogRecord = result?;
+            println!("{:?}", record);
+        }
+
+        Ok(worklog)
+    }
+
+    pub fn from_csv_file(filename: &str)  -> Result<Self, Box<dyn std::error::Error>> {
+        use std::fs::File;
+        use std::io::BufReader;
+        let file = File::open(&filename)?;
+        let mut buf_reader = BufReader::new(file);
+        Self::from_csv(buf_reader)
+    }
+}
 
 
 
+use clap::Parser;
 
 
 #[derive(Parser, Debug)]
 #[command(author="Michael Winkelmann", version, about="Invoicer")]
 struct Arguments{
-    #[arg(long, default_value_t = String::from(""))]
+    #[arg(long, default_value_t = String::new())]
     worklog_csv: String,
-    #[arg(long, default_value_t = String::from(""))]
+    #[arg(long, default_value_t = String::new())]
     invoicee_toml: String,
-    #[arg(short, long, default_value_t = String::from("/home/pi/data"))]
-    my_address: String,
+    #[arg(short, long, default_value_t = String::from("invoicer.toml"))]
+    config: String,
 }
 
 
 
 fn main() {
     println!("Hello, world!");
+
+    let worklog = Worklog::from_csv_file("examples/ExampleWorklog.csv").unwrap();
+
+
 }
